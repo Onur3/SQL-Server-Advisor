0937c1b60f4bd5e4f868e3d3cbbf4017dff5e185
Onur Efiloğlu <83568648+Onur3@users.noreply.github.com>
2026-09-09 15:00:29 +0300

    Add telemetry route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { DashboardComponent } from './features/dashboard/dashboard.component';
 import { FindingsComponent } from './features/findings/findings.component';
 import { RecommendationsComponent } from './features/recommendations/recommendations.component';
 import { ServersComponent } from './features/servers/servers.component';
+import { TelemetryComponent } from './features/telemetry/telemetry.component';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', component: DashboardComponent },
   { path: 'servers', component: ServersComponent },
+  { path: 'telemetry', component: TelemetryComponent },
   { path: 'findings', component: FindingsComponent },
   { path: 'recommendations', component: RecommendationsComponent },
   { path: '**', redirectTo: '' }

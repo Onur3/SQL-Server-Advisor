46df0af940c6b51986a7562d3cd30add69ea11be
Onur Efiloğlu <83568648+Onur3@users.noreply.github.com>
2026-09-09 14:58:36 +0300

    Align wait and blocking telemetry schema

---
+++ database/005_wait_blocking_schema_alignment.sql
@@ -0,0 +1,51 @@
+/* SQL Server Advisor - wait/blocking telemetry schema alignment
+   SQL Server 2019+ compatible and idempotent. */
+
+USE [SQLAdvisor];
+GO
+
+IF COL_LENGTH(N'SNP.Wait', N'SignalWaitTimeMs') IS NULL
+BEGIN
+    ALTER TABLE SNP.Wait
+        ADD SignalWaitTimeMs bigint NOT NULL
+            CONSTRAINT DF_SNP_Wait_SignalWaitTimeMs DEFAULT (0);
+
+    IF COL_LENGTH(N'SNP.Wait', N'SignalWaitMs') IS NOT NULL
+        UPDATE SNP.Wait SET SignalWaitTimeMs = ISNULL(SignalWaitMs, 0);
+END
+GO
+
+IF COL_LENGTH(N'SNP.Wait', N'DeltaSignalWaitTimeMs') IS NULL
+BEGIN
+    ALTER TABLE SNP.Wait
+        ADD DeltaSignalWaitTimeMs bigint NOT NULL
+            CONSTRAINT DF_SNP_Wait_DeltaSignalWaitTimeMs DEFAULT (0);
+
+    IF COL_LENGTH(N'SNP.Wait', N'DeltaSignalWaitMs') IS NOT NULL
+        UPDATE SNP.Wait SET DeltaSignalWaitTimeMs = ISNULL(DeltaSignalWaitMs, 0);
+END
+GO
+
+IF COL_LENGTH(N'SNP.Wait', N'DeltaWaitTimeMs') IS NOT NULL
+BEGIN
+    UPDATE SNP.Wait SET DeltaWaitTimeMs = 0 WHERE DeltaWaitTimeMs IS NULL;
+    ALTER TABLE SNP.Wait ALTER COLUMN DeltaWaitTimeMs bigint NOT NULL;
+END
+GO
+
+IF COL_LENGTH(N'EVT.Blocking', N'BlockingSessionId') IS NOT NULL
+BEGIN
+    UPDATE EVT.Blocking SET BlockingSessionId = 0 WHERE BlockingSessionId IS NULL;
+    ALTER TABLE EVT.Blocking ALTER COLUMN BlockingSessionId smallint NOT NULL;
+END
+GO
+
+IF COL_LENGTH(N'EVT.Blocking', N'WaitTimeMs') IS NOT NULL
+BEGIN
+    UPDATE EVT.Blocking SET WaitTimeMs = 0 WHERE WaitTimeMs IS NULL;
+    ALTER TABLE EVT.Blocking ALTER COLUMN WaitTimeMs bigint NOT NULL;
+END
+GO
+
+PRINT N'Wait and blocking telemetry schema alignment ready.';
+GO

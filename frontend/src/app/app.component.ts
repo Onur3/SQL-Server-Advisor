7d420b8cb94ef935aa5d31edc52faf02408a9eef
Onur Efiloğlu <83568648+Onur3@users.noreply.github.com>
2026-09-09 15:00:52 +0300

    Add wait and blocking navigation

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -26,6 +26,9 @@ import { MatIconModule } from '@angular/material/icon';
           <a mat-list-item routerLink="/servers" routerLinkActive="active">
             <mat-icon matListItemIcon>dns</mat-icon><span matListItemTitle>SQL Sunucuları</span>
           </a>
+          <a mat-list-item routerLink="/telemetry" routerLinkActive="active">
+            <mat-icon matListItemIcon>monitor_heart</mat-icon><span matListItemTitle>Wait & Blocking</span>
+          </a>
           <a mat-list-item class="disabled"><mat-icon matListItemIcon>query_stats</mat-icon><span matListItemTitle>Sorgular</span></a>
           <a mat-list-item routerLink="/findings" routerLinkActive="active">
             <mat-icon matListItemIcon>rule</mat-icon><span matListItemTitle>Bulgular</span>
